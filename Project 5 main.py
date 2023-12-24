d2e6186cf7a57196e0b3c0ca87618734f0bb4243
Baviyashree <143431286+Baviyashree@users.noreply.github.com>
2023-12-24 19:25:28 +0530

    Update and rename server.py to Project 5 main.py

--- Project 5 main.py
+++ Project 5 main.py
@@ -1,28 +1,23 @@
 import socket
 import threading
-
-HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
-
+HOST = '127.0.0.1'  
+PORT = 65432       
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((HOST, PORT))
 server.listen()
-
 clients = []
-
-def handle_client(client_socket):
+def handle(client_socket):
     while True:
         try:
             message = client_socket.recv(1024).decode('utf-8')
-            broadcast(message, client_socket)
+            broad(message, client_socket)
         except:
             index = clients.index(client_socket)
             clients.remove(client_socket)
             client_socket.close()
             print(f"Client {index} disconnected")
             break
-
-def broadcast(message, sender):
+def broad(message, sender):
     for client in clients:
         if client != sender:
             try:
@@ -30,10 +25,9 @@ def broadcast(message, sender):
             except:
                 client.close()
                 clients.remove(client)
-
 while True:
     client_socket, address = server.accept()
     print(f"Client {len(clients)} connected")
     clients.append(client_socket)
-    thread = threading.Thread(target=handle_client, args=(client_socket,))
+    thread = threading.Thread(target=handle, args=(client_socket,))
     thread.start()
