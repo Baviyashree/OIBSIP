d304c42ae0a83a5a1440624c2e05c79b3750f93d
Baviyashree <143431286+Baviyashree@users.noreply.github.com>
2023-12-24 19:19:15 +0530

    Update Project 5.py

--- Project 5.py
+++ Project 5.py
@@ -1,12 +1,9 @@
 import socket
 import threading
-
-HOST = '127.0.0.1'  # The server's hostname or IP address
-PORT = 65432        # The port used by the server
-
+HOST = '127.0.0.1' 
+PORT = 65432       
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((HOST, PORT))
-
 def receive_messages():
     while True:
         try:
@@ -15,10 +12,8 @@ def receive_messages():
         except:
             print("Connection closed")
             break
-
 receive_thread = threading.Thread(target=receive_messages)
 receive_thread.start()
-
 while True:
     message = input()
     client_socket.send(message.encode('utf-8'))
